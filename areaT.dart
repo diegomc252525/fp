d3c7d52b99480ce1bde15d0e6f016671c4468653
diegomc252525 <>
2021-04-26 13:41:44 +0000

    clase 3

--- areaT.dart
+++ areaT.dart
@@ -1,11 +1,14 @@
-#import("dart:io"); 
-
-main() { 
-    var stream = new StringInputStream(stdin); 
-    stream.onLine =() { 
-    var line = stream.readLine(); 
-    if (line != null) { 
-     print(line); 
-    } 
-    }; 
+import 'dart:io';
+void main() {
+ //Definir Variables y otros
+ print('Ejercicio 01: Area Triangulo');
+ //Datos de Entrada
+ print("Ingrese Base: ");
+ var b=int.parse(stdin.readLineSync());
+ print("Ingrese Altura: ");
+ var h=int.parse(stdin.readLineSync());
+ //Proceso
+ var area=(b*h)/2;
+ //Datos de Salida
+ print("El Area de Triangulo es: $area");
 }
